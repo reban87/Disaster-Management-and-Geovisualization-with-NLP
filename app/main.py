8911a698a152f4dac283625a8a47192f3c0417ce
AryalReban <reban@rpalabs.io>
2024-05-28 14:25:22 +0545

    updated logic

--- app/main.py
+++ app/main.py
@@ -35,4 +35,4 @@ async def extract(request: TextRequest):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, debug=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
---
+++ test.py
@@ -0,0 +1,382 @@
+import streamlit as st
+import pandas as pd
+import requests
+import moment
+from io import StringIO
+from visualise import generate_disaster_map
+from streamlit_folium import folium_static
+
+# Set up the page layout and title
+st.set_page_config(page_title="Enhanced Text Analysis Tool", layout="wide")
+st.title("Text Analysis and Visualization App")
+
+# Initialize session state variables for results and map generation
+if "classification_result" not in st.session_state:
+    st.session_state["classification_result"] = ""
+if "location" not in st.session_state:
+    st.session_state["location"] = ""
+if "raw_date" not in st.session_state:
+    st.session_state["raw_date"] = ""
+if "normalised_date" not in st.session_state:
+    st.session_state["normalised_date"] = ""
+if "hazard_type" not in st.session_state:
+    st.session_state["hazard_type"] = ""
+if "map_generated" not in st.session_state:
+    st.session_state["map_generated"] = False
+
+# Sidebar for input and controls
+with st.sidebar:
+    st.subheader("Input Text")
+    user_input = st.text_area("Enter your text here:", height=200)
+    classify_button = st.button("Classify Text")
+    ner_button = st.button("Extract Entities")
+    uploaded_file = st.file_uploader("Upload CSV file", type="csv")
+    if uploaded_file:
+        data = pd.read_csv(uploaded_file)
+        if "text" in data.columns:
+            process_button = st.button("Process Uploaded Texts")
+        else:
+            st.error("CSV must have a column named 'text'.")
+
+# District in Nepal
+districts_in_nepal = [
+    "Achham",
+    "Arghakhanchi",
+    "Baglung",
+    "Baitadi",
+    "Bajhang",
+    "Bajura",
+    "Banke",
+    "Bara",
+    "Bardiya",
+    "Bhaktapur",
+    "Bhojpur",
+    "Chitwan",
+    "Dadeldhura",
+    "Dailekh",
+    "Dang Deukhuri",
+    "Darchula",
+    "Dhading",
+    "Dhankuta",
+    "Dhanusa",
+    "Dolakha",
+    "Dolpa",
+    "Doti",
+    "Rukum East",
+    "Gorkha",
+    "Gulmi",
+    "Humla",
+    "Ilam",
+    "Jajarkot",
+    "Jhapa",
+    "Jumla",
+    "Kailali",
+    "Kalikot",
+    "Kanchanpur",
+    "Kapilvastu",
+    "Kaski",
+    "Kathmandu",
+    "Kavrepalanchok",
+    "Khotang",
+    "Lalitpur",
+    "Lamjung",
+    "Mahottari",
+    "Makwanpur",
+    "Manang",
+    "Morang",
+    "Mugu",
+    "Mustang",
+    "Myagdi",
+    "Nawalparasi East",
+    "Nawalpur",
+    "Nuwakot",
+    "Okhaldhunga",
+    "Palpa",
+    "Panchthar",
+    "Parasi",
+    "Parbat",
+    "Parsa",
+    "Pyuthan",
+    "Ramechhap",
+    "Rasuwa",
+    "Rautahat",
+    "Rolpa",
+    "Rupandehi",
+    "Salyan",
+    "Sankhuwasabha",
+    "Saptari",
+    "Sarlahi",
+    "Sindhuli",
+    "Sindhupalchok",
+    "Siraha",
+    "Solukhumbu",
+    "Sunsari",
+    "Surkhet",
+    "Syangja",
+    "Tanahun",
+    "Taplejung",
+    "Terhathum",
+    "Udayapur",
+    "Western Rukum",
+]
+
+
+def is_district(location_part):
+    normalized_location_part = (
+        location_part.strip().lower()
+    )  # Normalize the input to lower case
+    return any(
+        district.lower() == normalized_location_part for district in districts_in_nepal
+    )
+
+
+# Function to call API endpoints
+def process_text(api_url, input_text):
+    response = requests.post(api_url, json={"text": input_text})
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+# Display classification result
+st.subheader("Classification Results")
+if classify_button:
+    response = process_text("http://0.0.0.0:5000/predict", user_input)
+    if response:
+        st.session_state["classification_result"] = response.get("result", "")
+    else:
+        st.session_state["classification_result"] = (
+            "Failed to get response from classification API."
+        )
+st.text_input(
+    "Disaster vs Non Disaster",
+    value=st.session_state["classification_result"],
+    placeholder="Classification",
+)
+
+# Display NER results
+st.subheader("NER Results")
+if ner_button:
+    response = process_text("http://0.0.0.0:5000/extract", user_input)
+    if response:
+        data = response
+        location_set = set(
+            filter(
+                lambda x: x != "nan", data["results"]["entities"].get("location", [])
+            )
+        )
+        st.session_state["location"] = ", ".join(location_set)
+
+        raw_dates = data["results"]["entities"].get("date", [])
+        st.session_state["raw_date"] = ", ".join(raw_dates)
+        formatted_dates = [moment.date(d).format("YYYY-MM-DD") for d in raw_dates]
+        st.session_state["normalised_date"] = ", ".join(set(formatted_dates))
+
+        hazard_type_set = set(data["results"]["entities"].get("hazard_type", []))
+        st.session_state["hazard_type"] = ", ".join(hazard_type_set)
+
+    else:
+        st.error("Failed to get response from NER extraction API.")
+
+# Text input fields for displaying NER results
+st.text_input(
+    "Location",
+    value=st.session_state["location"],
+    placeholder="Location will appear here.",
+)
+st.text_input(
+    "Date", value=st.session_state["raw_date"], placeholder="Date will appear here."
+)
+st.text_input(
+    "Normalised Date",
+    value=st.session_state["normalised_date"],
+    placeholder="Normalised date will appear here.",
+)
+st.text_input(
+    "Disaster Type",
+    value=st.session_state["hazard_type"],
+    placeholder="Disaster type will appear here.",
+)
+
+# Generate and display map
+if ner_button and st.session_state["location"] and st.session_state["hazard_type"]:
+    # Filtering valid districts
+    valid_districts = [
+        loc for loc in st.session_state["location"].split(", ") if is_district(loc)
+    ]
+    if valid_districts:
+        visualization_data = {
+            "location": ", ".join(valid_districts),
+            "date": st.session_state["normalised_date"],
+            "hazard_type": st.session_state["hazard_type"],
+        }
+        result_map = generate_disaster_map(
+            visualization_data, "Disaster Layer", "Disaster Data"
+        )
+        if result_map:
+            st.session_state["map_generated"] = True
+            folium_static(result_map)
+            st.success("Map has been successfully generated")
+    # if ner_button and location_set and hazard_type_set:
+    #     visualization_data = {
+    #         "location": st.session_state["location"],
+    #         "date": st.session_state["normalised_date"],
+    #         "hazard_type": st.session_state["hazard_type"],
+    #     }
+    #     result_map = generate_disaster_map(
+    #         visualization_data, "Disaster Layer", "Disaster Data"
+    #     )
+    #     if result_map:
+    #         st.session_state["map_generated"] = True
+    #         folium_static(result_map)
+    #         st.success("Map has been successfully generated")
+
+    # UNCOMMENT TO PREVIOUS VERSION
+    # if "process_button" in locals() and process_button:
+    # results = []
+    # map_folium = None  # We will create a single map and add all data to it
+    # for text in data["text"]:
+    #     classification_result = process_text("http://0.0.0.0:5000/predict", text)
+    #     ner_result = process_text("http://0.0.0.0:5000/extract", text)
+    #     if ner_result:
+    #         locations = ", ".join(
+    #             set(
+    #                 filter(
+    #                     lambda x: x != "nan",
+    #                     ner_result["results"]["entities"].get("location", []),
+    #                 )
+    #             )
+    #         )
+    #         dates = ", ".join(
+    #             set(
+    #                 [
+    #                     moment.date(d).format("YYYY-MM-DD")
+    #                     for d in ner_result["results"]["entities"].get("date", [])
+    #                 ]
+    #             )
+    #         )
+    #         hazard_types = ", ".join(
+    #             set(ner_result["results"]["entities"].get("hazard_type", []))
+    #         )
+
+    #         # Generate map for each location from CSV
+    #         for location, hazard_type, date in zip(
+    #             locations.split(", "), hazard_types.split(", "), dates.split(", ")
+    #         ):
+    #             map_data = {
+    #                 "location": location,
+    #                 "date": date,
+    #                 "hazard_type": hazard_type,
+    #             }
+    #             if not map_folium:
+    #                 map_folium = generate_disaster_map(
+    #                     map_data, "Disaster Layer", "Disaster Data"
+    #                 )
+    #             else:
+    #                 # Now adds to the existing map
+    #                 map_folium = generate_disaster_map(
+    #                     map_data,
+    #                     "Disaster Layer",
+    #                     "Disaster Data",
+    #                     existing_map=map_folium,
+    #                 )
+
+    #     result_entry = {
+    #         "text": text,
+    #         "classification": (
+    #             classification_result.get("result")
+    #             if classification_result
+    #             else "Error"
+    #         ),
+    #         "locations": locations,
+    #         "dates": dates,
+    #         "hazard_types": hazard_types,
+    #     }
+    #     results.append(result_entry)
+
+    # if map_folium:
+    #     folium_static(map_folium)  # Display the final map with all locations
+    # results_df = pd.DataFrame(results)
+    # st.download_button(
+    #     label="Download Results as CSV",
+    #     data=results_df.to_csv(index=False),
+    #     file_name="processed_results.csv",
+    #     mime="text/csv",
+    # )
+if "process_button" in locals() and process_button:
+    results = []
+    map_folium = None  # We will create a single map and add all data to it
+    for text in data["text"]:
+        classification_result = process_text("http://0.0.0.0:5000/predict", text)
+        ner_result = process_text("http://0.0.0.0:5000/extract", text)
+        print(f"ner_results: {ner_result}, type: {type(ner_result)}")
+
+        if ner_result:
+            print(
+                "Raw locations:", ner_result["results"]["entities"].get("location", [])
+            )
+            # Filter out non-district locations and 'nan' values
+            valid_locations = filter(
+                lambda x: x != "nan" and is_district(x),
+                ner_result["results"]["entities"].get("location", []),
+            )
+            locations = ", ".join(set(valid_locations))
+            print("location:", locations)
+            dates = ", ".join(
+                set(
+                    [
+                        moment.date(d).format("YYYY-MM-DD")
+                        for d in ner_result["results"]["entities"].get("date", [])
+                    ]
+                )
+            )
+            hazard_types = ", ".join(
+                set(ner_result["results"]["entities"].get("hazard_type", []))
+            )
+
+            # Generate map for each valid location from CSV
+            for location, hazard_type, date in zip(
+                locations.split(", "), hazard_types.split(", "), dates.split(", ")
+            ):
+                if location:  # Ensures that only valid locations are processed
+                    map_data = {
+                        "location": location,
+                        "date": date,
+                        "hazard_type": hazard_type,
+                    }
+                    if not map_folium:
+                        map_folium = generate_disaster_map(
+                            map_data, "Disaster Layer", "Disaster Data"
+                        )
+                    else:
+                        # Now adds to the existing map
+                        map_folium = generate_disaster_map(
+                            map_data,
+                            "Disaster Layer",
+                            "Disaster Data",
+                            existing_map=map_folium,
+                        )
+
+        result_entry = {
+            "text": text,
+            "classification": (
+                classification_result.get("result")
+                if classification_result
+                else "Error"
+            ),
+            "locations": locations,
+            "dates": dates,
+            "hazard_types": hazard_types,
+        }
+        results.append(result_entry)
+
+    if map_folium:
+        folium_static(map_folium)  # Display the final map with all locations
+    results_df = pd.DataFrame(results)
+    st.download_button(
+        label="Download Results as CSV",
+        data=results_df.to_csv(index=False),
+        file_name="processed_results.csv",
+        mime="text/csv",
+    )
